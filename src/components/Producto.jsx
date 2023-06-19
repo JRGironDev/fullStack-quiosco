49daa019ee156ae9f614be42dddf60e02a854bc6
Jose Quiñónez <joserobertoq@outlook.com>
2023-06-19 05:32:08 -0600

    Obteniendo los productos

---
+++ src/components/Producto.jsx
@@ -0,0 +1,3 @@
+export default function Producto({ producto }) {
+  return <div>Producto</div>;
+}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,7 @@ export default function Sidebar() {
 
       <div className="mt-10">
         {categorias.map((categoria) => (
-          <Categoria categoria={categoria} />
+          <Categoria key={categoria.id} categoria={categoria} />
         ))}
       </div>
 
