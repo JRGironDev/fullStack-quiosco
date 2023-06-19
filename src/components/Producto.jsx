b250671252fb0da11ad97eaeb7903c38bde19d80
Jose Quiñónez <joserobertoq@outlook.com>
2023-06-19 06:02:26 -0600

    Finalizando panel de productos

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,3 +1,5 @@
+import { formatearDinero } from "../helpers";
+
 export default function Producto({ producto }) {
   const { nombre, imagen, precio } = producto;
 
@@ -9,8 +11,16 @@ export default function Producto({ producto }) {
         alt={`imagen ${nombre}`}
       />
       <div className="p-5">
-        <h3 className="text-2xl font-bold ">{nombre}</h3>
-        <p className="mt-5 font-black text-4xl text-amber-500 ">{precio}</p>
+        <h3 className="text-xl">{nombre}</h3>
+
+        <p className="mt-5 text-2xl text-amber-500 ">${precio}</p>
+
+        <button
+          type="button"
+          className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold"
+        >
+          Agregar
+        </button>
       </div>
     </div>
   );
