11dd674fc7d714710c7978758d4ca64695a65964
Jose Quiñónez <joserobertoq@outlook.com>
2023-06-19 05:39:51 -0600

    Mostrando los productos

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,3 +1,17 @@
 export default function Producto({ producto }) {
-  return <div>Producto</div>;
+  const { nombre, imagen, precio } = producto;
+
+  return (
+    <div className="border p-3 shadow bg-white">
+      <img
+        className="w-full"
+        src={`/img/${imagen}.jpg`}
+        alt={`imagen ${nombre}`}
+      />
+      <div className="p-5">
+        <h3 className="text-2xl font-bold ">{nombre}</h3>
+        <p className="mt-5 font-black text-4xl text-amber-500 ">{precio}</p>
+      </div>
+    </div>
+  );
 }
