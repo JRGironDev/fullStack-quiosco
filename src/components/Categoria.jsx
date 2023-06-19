393bc60f57f19a8d3b0d39188df88e3e31481f69
Jose Quiñónez <joserobertoq@outlook.com>
2023-06-19 05:16:34 -0600

    Finalizando el menú de Categoria

--- src/components/Categoria.jsx
+++ src/components/Categoria.jsx
@@ -1,3 +1,13 @@
 export default function Categoria({ categoria }) {
-  return <div>Categoria</div>;
+  const { icono, id, nombre } = categoria;
+  return (
+    <div className="flex items-center gap-7 w-100 p-4 hover:bg-amber-400 cursor-pointer">
+      <img
+        src={`/img/icono_${icono}.svg`}
+        alt="imagen icono"
+        className="w-10"
+      />
+      <p className="text-lg cursor-pointer truncate">{nombre}</p>
+    </div>
+  );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,13 +5,19 @@ export default function Sidebar() {
   return (
     <aside className="md:w-72">
       <div className="p-4">
-        <img src="img/logo.svg" alt="" className="w-40 " />
+        <img src="img/logo.svg" alt="logo" className="w-40 " />
       </div>
 
       <div className="mt-10">
         {categorias.map((categoria) => (
           <Categoria categoria={categoria} />
         ))}
+      </div>
+
+      <div className="m-5 px-5">
+        <button className="text-center bg-red-500 hover:bg-red-700 w-full p-3 font-bold text-white truncate">
+          Cancelar Orden
+        </button>
       </div>
     </aside>
   );
