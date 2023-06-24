55b8ac1747b79f290399bcd358116171513963c8
Jose Quiñónez <joserobertoq@outlook.com>
2023-06-24 08:42:55 -0600

    Mostrando imagen en modal

--- src/components/ModalProducto.jsx
+++ src/components/ModalProducto.jsx
@@ -1,7 +1,7 @@
 import useQuiosco from "../hooks/useQuiosco";
 
 export default function ModalProducto() {
-  const { producto } = useQuiosco();
+  const { producto, handleClickModal } = useQuiosco();
 
   return (
     <div className="md:flex gap-10">
@@ -11,7 +11,26 @@ export default function ModalProducto() {
           alt={`Imagen producto ${producto.nombre}`}
         />
       </div>
-      <div className="md:w-2/3"></div>
+      <div className="md:w-2/3">
+        <div className="flex justify-end items-center">
+          <button onClick={handleClickModal}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
